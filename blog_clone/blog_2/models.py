24b551b43934adc3b40e0151ffc02564cdbd386f
Sahejeets7 <Sahejeetsingh97@gmail.com>
2019-03-04 12:43:15 +0530

    Django portfolio,first commit

---
+++ blog_clone/blog_2/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from django.urls import reverse
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+
+    def get_absolute_url(self):
+        return reverse('post_detail',kwargs = {'pk':self.pk})  #that is going to be the list view
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comments(models.Model):
+    post = models.ForeignKey('Post',related_name='comments',on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')  #that is going to be the list view, whcih we r also going to use as the homepage!
+
+    def __str__(self):
+        return self.text
